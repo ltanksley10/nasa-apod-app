e699016cb4c3b9ce9ff827f1b063146f34fc0a93
ltanksley10 <contactlthere@gmail.com>
2021-05-03 22:57:37 +0000

    added animation to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,18 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div className="home">
-            <Link className="home-link" to="/mainpage"><h1>View NASA's Photo of the Day</h1></Link>
+        <div>
+            <div className="headline-flexAlign">
+                <div>
+                    <h1 className="headline">NASA's Astronomy Photo of the Day</h1>
+                </div>
+                <div>
+                    <img className="headline-img" src="https://img.icons8.com/plasticine/160/000000/rocket.png" alt="Rocketship icon" />
+                </div>
+            </div>
+            <div className="home">
+                <Link className="home-link" to="/mainpage"><h2>View Here</h2></Link>
+            </div>
         </div>
     );
 }
